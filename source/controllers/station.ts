5502c96e567c4798adc57c4b196f5354d55ffb8a
OwaisTariq99 <Owais@eforte.net>
2021-03-05 17:02:24 +0500

    added comments

--- source/controllers/station.ts
+++ source/controllers/station.ts
@@ -10,149 +10,130 @@ import config from '../config/config';
 
 /**
  * createStation - insert station in database.
- * @param req - request from parameters
- * @param res - response of the API
- * @param next - proceed to the next function
+ * @param object - object that needs to be saved
  * @returns {Promise<void>}
  */
-const createStation = (req: Request, res: Response, next: NextFunction) => {
-    fetch('https://www.rideindego.com/stations/json/')
-        .then((res) => res.json())
-        .catch(function (error) {
-            return res.status(400).json({
-                status: 400,
-                data: null,
-                error: error.message
-            });
-        })
-        .then(function (response) {
-            const station = new Station({
+const createStation = async (object: object) => {
+    try {
+      const station = new Station({
                 _id: new mongoose.Types.ObjectId(),
-                data: response
+                data: object
             });
-
-            return station
-                .save()
-                .then((result) => {
-                    return res.status(200).json({
-                        status: 200,
-                        data: result,
-                        error: null
-                    });
-                })
-                .catch((error) => {
-                    return res.status(500).json({
-                        status: 500,
-                        data: null,
-                        error: error.message
-                    });
-                });
-        });
+      await station.save();
+      return station;
+    } catch (error) {
+      return error;
+    }
 };
 
 /**
  * getWeatherAndStationData - Get weather and station data from API.
- * @param req - request from parameters
- * @param res - response of the API
- * @param next - proceed to the next function
- * @returns {Promise<void>}
+ * @param at - string that needs to be compared
+ * @returns {Promise}
  */
-const getWeatherAndStationData = (req: Request, res: Response, next: NextFunction) => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=' + config.auth.API_KEY)
-        .then((res) => res.json())
-        .catch(function (error) {
-            return res.status(400).json({
-                status: 400,
-                data: null,
-                error: error.message
-            });
-        })
-        .then(function (response) {
-            Station.findOne({ $or: [{ createdAt: req.params.at }, { createdAt: { $gt: req.params.at } }] })
-                .exec()
-                .then((stations) => {
-                    let obj = {
-                        at: req.params.at,
-                        stations: stations,
-                        weather: response
-                    };
-                    return res.status(200).json({
-                        obj: obj
-                    });
-                })
-                .catch((error) => {
-                    return res.status(500).json({
-                        message: null,
-                        error: error.message
-                    });
-                });
-        });
+const getWeatherAndStationData = async (at: String) => {
+    try {
+      let station = await Station.findOne({ $or: [{ createdAt: at }, { createdAt: { $gt: at } }] });
+      return station;
+    } catch (error) {
+      return error;
+    }
+    // fetch(link + config.auth.API_KEY)
+    //     .then((res) => res.json())
+    //     .catch(function (error) {
+    //         return error;
+    //     })
+    //     .then(function (response) {
+    //         Station.findOne({ $or: [{ createdAt: at }, { createdAt: { $gt: at } }] })
+    //             .exec()
+    //             .then((stations) => {
+    //                 let obj = {
+    //                     at: at,
+    //                     stations: stations,
+    //                     weather: response
+    //                 };
+    //                 return obj ;
+    //             })
+    //             .catch((error) => {
+    //                 return error;
+    //             });
+    //     });
 };
 
 /**
  * getWeatherAndStationDataByKioskId - Get weather and station data by its kioski id.
- * @param req - request from parameters
- * @param res - response of the API
- * @param next - proceed to the next function
- * @returns {Promise<void>}
+ * @param at - string that needs to be compared
+ * @returns {Promise}
  */
-const getWeatherAndStationDataByKioskId = (req: Request, res: Response, next: NextFunction) => {
+const getWeatherAndStationDataByKioskId = async (at: String, id: any) => {
     try {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=' + config.auth.API_KEY)
-            .then((res) => res.json())
-            .catch(function (error) {
-                return res.status(400).json({
-                    status: 400,
-                    data: null,
-                    error: error.message
-                });
-            })
-            .then(function (response) {
-                Station.findOne({ $or: [{ createdAt: req.params.at }, { createdAt: { $gt: req.params.at } }] })
-                    .exec()
-                    .then((stations) => {
-                        let obj = {};
-                        let check = 0;
-                        for (let i = 0; i < stations?.data.features.length; i++) {
-                            if (stations?.data.features[i].properties.kioskId == req.params.kioskId) {
-                                obj = {
-                                    at: req.params.at,
-                                    station: stations?.data.features[i],
-                                    weather: response
-                                };
-                                check = 1;
-                                break;
-                            }
-                        }
-                        if (check == 1) {
-                            return res.status(200).json({
-                                obj: obj
-                            });
-                        } else {
-                            return res.status(404).json({
-                                message: null,
-                                error: 'Cannot find data'
-                            });
-                        }
-                    })
-                    .catch((error) => {
-                        return res.status(500).json({
-                            message: null,
-                            error: error.message
-                        });
-                    });
-            })
-            .catch((error) => {
-                return res.status(500).json({
-                    message: null,
-                    error: error.message
-                });
-            });
+        let stations = await Station.findOne({ $or: [{ createdAt: at }, { createdAt: { $gt: at } }] });
+        let check: Number = 0;
+        let specficStation: object= {};
+        for (let i = 0; i < stations?.data.features.length; i++) {
+            if (stations?.data.features[i].properties.kioskId == id) {
+                specficStation = stations?.data.features[i];
+                check = 1;
+                break;
+            }
+        }
+        if (check === 1) {
+            return specficStation
+        } else {
+            return {error: 'Cannot find data'}
+        }
+        // fetch('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=' + config.auth.API_KEY)
+        //     .then((res) => res.json())
+        //     .catch(function (error) {
+        //         return res.status(400).json({
+        //             status: 400,
+        //             data: null,
+        //             error: error.message
+        //         });
+        //     })
+        //     .then(function (response) {
+        //         Station.findOne({ $or: [{ createdAt: req.params.at }, { createdAt: { $gt: req.params.at } }] })
+        //             .exec()
+        //             .then((stations) => {
+        //                 let obj = {};
+                        // let check = 0;
+                        // for (let i = 0; i < stations?.data.features.length; i++) {
+                        //     if (stations?.data.features[i].properties.kioskId == req.params.kioskId) {
+                        //         obj = {
+                        //             at: req.params.at,
+                        //             station: stations?.data.features[i],
+                        //             weather: response
+                        //         };
+                        //         check = 1;
+                        //         break;
+                        //     }
+                        // }
+                        // if (check == 1) {
+                        //     return res.status(200).json({
+                        //         obj: obj
+                        //     });
+                        // } else {
+                        //     return res.status(404).json({
+                        //         message: null,
+                        //         error: 'Cannot find data'
+                        //     });
+                        // }
+        //             })
+        //             .catch((error) => {
+        //                 return res.status(500).json({
+        //                     message: null,
+        //                     error: error.message
+        //                 });
+        //             });
+        //     })
+        //     .catch((error) => {
+        //         return res.status(500).json({
+        //             message: null,
+        //             error: error.message
+        //         });
+        //     });
     } catch (err) {
-        return res.status(500).json({
-            message: null,
-            error: err.message
-        });
+        return err
     }
 };
 
---
+++ test/station.test.ts
@@ -0,0 +1,35 @@
+// import controller from '../source/controllers/station';
+// import chai from 'chai';
+// chai.use(require('chai-fs'));
+// // import sinon from 'sinon';
+// const expect = require('chai').expect
+// // import fetch from 'node-fetch';
+// // import verifyToken from '../source/services/verifyToken';
+
+// describe('station insertion unit test case', async () => {
+//     it('should return error from user station', function (done) {
+//         let Obj = {};
+//         controller.createStation(Obj)
+//             .then((res: any) => {
+//                 // never called
+//             })
+//             .catch((error: any) => {
+//                 expect(error).to.be.not.equal(undefined);
+//                 expect(error).to.be.equal(true);
+//                 done();
+//             });
+//     });
+//     it('should return user from station save', function (done) {
+//         let Obj = {};
+//         controller.createStation(Obj)
+//             .then((res: any) => {
+//                 expect(res).to.be.not.equal(undefined);
+//                 expect(res).to.be.not.equal(null);
+//                 expect(res.Obj).to.be.an('object');
+//                 done();
+//             })
+//             .catch((error: any) => {
+//                 // never called
+//             });
+//     });
+// });
